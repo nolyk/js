e9291aeeed03525666393db662d2889970f1c029
nolyk <146582927+nolyk@users.noreply.github.com>
2025-01-06 20:48:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = prompt("Imsingizni Kiriting")
+let surname = prompt("Familiyangizni Kiriting")
+let age = prompt("Yoshingizni Kiriting")
+let work = prompt("Yoqtirgan faningizni yozing", 'IT')
+let address = prompt("Yashaydigan tumaningizni kiriting")
+alert("Malumotlaringizni kiritganiz uchun tashakkur")
+console.log("Ism:", name);
+console.log("Familiya:", surname);
+console.log("Yosh:", age);
+console.log("Yoqtirgan fan:", work);
+console.log("Yashash manzili:", address);
